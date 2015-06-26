c41859450d74b7885649316347d2905ed72f9d90
Johann Tuffe <tafia973@gmail.com>
2015-06-26 08:35:38 +0800

    split into modules

---
+++ src/search_function.rs
@@ -0,0 +1,19 @@
+use std::io::{BufRead, BufReader, Result};
+use std::fs::File;
+use regex::Regex;
+use std::str::from_utf8;
+
+static REGEX_LET: Regex = regex!(r"(?:if\s+)?let\s+(\w+)\s+=");
+
+#[derive(Debug,Clone,PartialEq)]
+struct Token {
+	name: String,  	// match name
+	pos: usize 		// position in the file
+}
+
+struct SearchFnIter {
+    file: BufReader<File>,
+    pos: usize,
+    skip: Option<(u8, u8)>,
+    buf: String
+}
