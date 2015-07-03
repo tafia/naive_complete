6b7804627a726e76b0271d65baa00f3a423a354d
Johann Tuffe <tafia973@gmail.com>
2015-07-03 16:20:58 +0800

    Simplify func_parser
    - rename `SearchFnIter` into `FnParser`
    - rename `open` into `new`
    - rename `Request`/`request` into `Scope`/`scope`
    - simplify iterator using simple `rfind`
    
    Not tested !

--- src/func_parser.rs
+++ src/func_parser.rs
@@ -1,47 +1,47 @@
 use std::io::{Read, Error};
 use std::fs::File;
-use regex::Regex;
+//use regex::Regex;
 
 use super::Token;
 
-static REGEX_DECL: Regex = regex!(r"(?:let\s|\()\s*(\w+)(?:\s*:\s*(\w+))?");
+//static REGEX_DECL: Regex = regex!(r"(?:let\s|\()\s*(\w+)(?:\s*:\s*(\w+))?");
 
 #[derive(Debug,Clone,PartialEq)]
-pub enum Request {
-	Path(Vec<Token>),
-	Fn(Vec<Token>),
-	Word(Token)
+pub enum Scope {
+    Path(Vec<Token>),
+    Fn(Vec<Token>),
+    Word(Token)
 }
 
-pub struct SearchFnIter {
-    pos: usize,
+pub struct FnParser {
+    start: usize,
     buf: String
 }
 
-impl SearchFnIter {
+impl FnParser {
 
-    pub fn open(path: &str, offset: usize, pos: usize) ->  Result<SearchFnIter, Error> {
-        debug!("creating SearchFnIter with on {}, {}", path, pos);
+    pub fn new(path: &str, offset: usize, pos: usize) ->  Result<FnParser, Error> {
+        debug!("creating FnParser with on {}, {}", path, pos);
         let file = try!(File::open(path));
         let buf = file.chars().into_iter()
-        	.skip(offset)
-        	.map(|c| c.unwrap())
-        	.enumerate()
-		  	.take_while(|&(i, c)| 
-		  		i < pos - offset || 
-		  		c.is_alphabetic() || 
-		  		c.is_numeric() ||
-		  		c == '_')
-		  	.map(|(_, c)| c).collect::<String>();
+                  .skip(offset)
+                  .map(|c| c.unwrap())
+                  .enumerate()
+                  .take_while(|&(i, c)|
+                      i < pos - offset ||
+                      c.is_alphabetic() ||
+                      c.is_numeric() ||
+                      c == '_')
+                  .map(|(_, c)| c).collect::<String>();
 		
-		debug!("buffer: {}", &buf);
-		Ok(SearchFnIter {
-            pos: offset,
+        debug!("buffer: {}", &buf);
+        Ok(FnParser {
+            start: offset,
             buf: buf
-		})
+        })
     }
 
-    pub fn request(&self) -> Request {
+    pub fn scope(&self) -> Scope {
 
     	let ifn = self.buf.rfind('.').unwrap_or(0);
     	let ipath = self.buf.rfind(':').unwrap_or(0);
@@ -50,54 +50,51 @@ impl SearchFnIter {
     		_ => false
     	}).unwrap_or(0);
 
-		if ifn > ipath {
-			if ifn > iword {
-				Request::Fn(self.buf[iword..].split('.')
-					.map(|s| Token { name: s.to_string(), pos: self.pos + ifn })
-					.collect())
-			} else {
-				Request::Word(Token { name: self.buf[iword..].to_string(), pos: self.pos + iword })
-			}
-		} else {
-			if ipath > iword {
-				Request::Path(self.buf[iword..].split("::")
-					.map(|s| Token { name: s.to_string(), pos: self.pos + ipath })
-					.collect())
-			} else {
-				Request::Word(Token { name: self.buf[iword..].to_string(), pos: self.pos + iword })
-			}
-		}
+        if ifn > ipath {
+            if ifn > iword {
+                Scope::Fn(self.buf[iword..].split('.')
+                    .map(|s| Token { name: s.to_string(), pos: self.start + ifn })
+                    .collect())
+            } else {
+                Scope::Word(Token { name: self.buf[iword..].to_string(), pos: self.start + iword })
+            }
+        } else {
+            if ipath > iword {
+                Scope::Path(self.buf[iword..].split("::")
+                    .map(|s| Token { name: s.to_string(), pos: self.start + ipath })
+                    .collect())
+            } else {
+                Scope::Word(Token { name: self.buf[iword..].to_string(), pos: self.start + iword })
+            }
+        }
+    }
+    
+    pub fn iter(&'a self, name: &'a str, end: usize) -> FnIter<'a> {
+    	let buf_end = if end < self.start { 0 } else { end - self.start };
+    	FnIter {
+    	   inner: &self,
+    	   name: name,
+    	   buf_end: buf_end
+    	}
     }
 }
 
-impl Iterator for SearchFnIter {
-    type Item = (Token, Token); // name, type
+struct FnIter<'a> {
+    inner: &'a FnParser,
+    name: &'a str,
+    buf_end: usize
+}
 
-    fn next(&mut self) -> Option<(Token, Token)> {
+impl<'a> Iterator for FnIter<'a> {
+    type Item = Token;
 
-        if let Some(caps) = REGEX_DECL.captures(&self.buf[self.pos..]) {
+    fn next(&mut self) -> Option<Token> {
 
-            let (start, end) = caps.pos(1).unwrap();
-            let name = Token { 
-            	name: self.buf[self.pos+start..self.pos+end].to_string(), 
-            	pos: self.pos+start 
-            };
-
-            let typ = if let Some((s, e)) = caps.pos(2) {
-            	let pos = self.pos;
-            	self.pos += e;
-				Token { 
-            		name: self.buf[pos+s..pos+e].to_string(), 
-            		pos: pos+s
-            	}
-            } else {
-            	self.pos += end;
-            	Token { name: String::new(), pos: self.pos }
-            };
-
-            Some((name, typ))
-        } else {
-        	None
-        }
+    	// TODO: make a better search !
+    	// TODO: extend until end of found word
+    	self.inner.buf[..self.buf_end].rfind(self.name).map(|n| {
+    	    self.buf_end = n;
+    	    Token { name: self.name.to_string(), pos: self.inner.start + self.buf_end }
+    	})
     }
 }
