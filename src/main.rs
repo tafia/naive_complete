a4ea944e0985cc4383ee2aa7e8543547fad03633
Johann Tuffe <tafia973@gmail.com>
2015-07-07 23:03:28 +0800

    remove brain file, use main directly

--- src/main.rs
+++ src/main.rs
@@ -10,24 +10,13 @@ extern crate toml;
 mod file_parser;
 mod func_parser;
 mod file_searcher;
-mod brain;
 
-use brain::prefix;
+use func_parser::{FnParser, Scope};
 
 #[derive(Debug,Clone,PartialEq)]
 pub struct Token {
     pub name: String,      // match name
     pub pos: usize         // position in the file
-}
-
-#[cfg(not(test))]
-pub fn print_usage() {
-    let program = &std::env::args().next().unwrap();
-    println!("usage: {} complete linenum charnum fname", program);
-    println!("or:    {} find-definition pos fname", program);
-    println!("or:    {} complete fullyqualifiedname   (e.g. std::io::)", program);
-    println!("or:    {} prefix pos fname", program);
-    println!("or replace complete with complete-with-snippet for more detailed completions.");
 }
 
 fn main() {
@@ -74,4 +63,45 @@ fn main() {
     //     },
     //     _ => println!("too many arguments")
     // }
+}
+
+#[cfg(not(test))]
+fn print_usage() {
+    let program = &std::env::args().next().unwrap();
+    println!("usage: {} complete linenum charnum fname", program);
+    println!("or:    {} find-definition pos fname", program);
+    println!("or:    {} complete fullyqualifiedname   (e.g. std::io::)", program);
+    println!("or:    {} prefix pos fname", program);
+    println!("or replace complete with complete-with-snippet for more detailed completions.");
+}
+
+fn prefix(args: &Vec<String>) {
+	 if args.len() != 3 {
+        println!("Cannot run 'prefix', expect 3 arguments, found {}", args.len());
+        print_usage();
+    } else {
+    	let pos = args[1].parse::<usize>().unwrap();
+        let parser = FnParser::new(&args[2], 0, pos).unwrap();
+        let name = match parser.scope() {
+            Scope::Path(segments) => {
+                if segments.len() == 0 {
+                    println!("cannot find scope!");
+                    return;
+                }
+                segments[0].clone()
+            }
+            Scope::Fn(segments) => {
+                if segments.len() == 0 {
+                    println!("cannot find scope!");
+                    return;
+                }
+                segments[0].clone()
+            }
+            Scope::Word(word) => word,
+        };
+        println!("scope: {:?}", name);
+        for it in parser.iter(&name.name, name.pos) {
+            println!("fn item {:?}", it);
+        }
+    }
 }
